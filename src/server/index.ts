6ef502cd80c35900f517415966e956d9b05c2a9d
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-11-11 22:43:53 -0300

    chore: configures tests environment with `vitest`

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,8 +5,13 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/sign-up', routeAdapter(makeSignUpController()));
+app.post(mountUrl('/sign-up'), routeAdapter(makeSignUpController()));
 
 app.listen(3000, () => {
 	console.log('🚀 Server is running on port: http://localhost:3000');
 });
+
+function mountUrl(path: string) {
+	const baseRoute = '/api/v1';
+	return baseRoute.concat(path);
+}
