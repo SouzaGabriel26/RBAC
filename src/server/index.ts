ab4836c8996c0e8ccdcf63e5f1156db7853c8f3a
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-11-12 22:43:36 -0300

    feat(`SignInController`): creates `SignInController` to handle the request and execute `signInUseCase`

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,17 +1,19 @@
-import express from 'express';
-import { makeSignUpController } from '../factories/makeSignUpController';
-import { routeAdapter } from './adapters/routeAdapter';
+import express from "express";
+import { makeSignInController } from "../factories/makeSignInController";
+import { makeSignUpController } from "../factories/makeSignUpController";
+import { routeAdapter } from "./adapters/routeAdapter";
 const app = express();
 
 app.use(express.json());
 
-app.post(mountUrl('/sign-up'), routeAdapter(makeSignUpController()));
+app.post(mountUrl("/sign-up"), routeAdapter(makeSignUpController()));
+app.post(mountUrl("/sign-in"), routeAdapter(makeSignInController()));
 
 app.listen(3000, () => {
-	console.log('🚀 Server is running on port: http://localhost:3000');
+  console.log("🚀 Server is running on port: http://localhost:3000");
 });
 
 function mountUrl(path: string) {
-	const baseRoute = '/api/v1';
-	return baseRoute.concat(path);
+  const baseRoute = "/api/v1";
+  return baseRoute.concat(path);
 }
