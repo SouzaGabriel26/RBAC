a3b6390a75a796828b855a145759e7fbcf856684
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-11-13 22:20:24 -0300

    feat: creates `ListLeadsController` returning mocked data to simulate a private route

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import { makeAuthenticationMiddleware } from "../factories/makeAuthenticationMiddleware";
+import { makeListLeadsController } from "../factories/makeListLeadsController";
 import { makeSignInController } from "../factories/makeSignInController";
 import { makeSignUpController } from "../factories/makeSignUpController";
+import { middlewareAdapter } from "./adapters/middlewareAdapter";
 import { routeAdapter } from "./adapters/routeAdapter";
 const app = express();
 
@@ -8,6 +11,12 @@ app.use(express.json());
 
 app.post(mountUrl("/sign-up"), routeAdapter(makeSignUpController()));
 app.post(mountUrl("/sign-in"), routeAdapter(makeSignInController()));
+
+app.get(
+  mountUrl("/leads"),
+  middlewareAdapter(makeAuthenticationMiddleware()),
+  routeAdapter(makeListLeadsController())
+);
 
 app.listen(3000, () => {
   console.log("🚀 Server is running on port: http://localhost:3000");
